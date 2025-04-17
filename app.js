55c32de886351e1250a775972943253bb5f9fe41
muhid <shaikhmuhid165@gmail.com>
2025-04-17 16:29:44 +0530

    applid neccessary changes in the app.js file

--- app.js
+++ app.js
@@ -8,6 +8,18 @@ const cors = require("cors");
 const errorMiddleware = require("./middlewares/error");
 dotenv.config({ path: "./data/config.env" });
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173"); // or your frontend URL
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT,DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200); // handles preflight
+  }
+
+  next();
+});
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/v1/users", userRouter);
@@ -21,22 +33,10 @@ app.use(
   })
 );
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173"); // or your frontend URL
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT,DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200); // handles preflight
-  }
-
-  next();
-});
 
 
 app.get("/", (req, res) => {
-  res.send("<h1>Code changed from github in masterbranch for CORS issue 2nd TIME </h1>");
+  res.send("<h1>Code changed from github [git push origin master] </h1>");
 });
 
 
