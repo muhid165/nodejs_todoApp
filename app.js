cce1fa7083538864744c8424449e53d3fea54e5d
muhiDroid165 <121967013+muhid165@users.noreply.github.com>
2025-04-17 12:10:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,6 +21,20 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173"); // or your frontend URL
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT ,DELETE ,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200); // handles preflight
+  }
+
+  next();
+});
+
+
 app.get("/", (req, res) => {
   res.send("<h1>Code changed from github in masterbranch for CORS issue </h1>");
 });
